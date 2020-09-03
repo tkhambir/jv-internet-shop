2b2300d49911504ecf2ddb3e56dc5f9596f7f497
tkhambir <tatiana.khambir@gmail.com>
2020-09-03 15:09:05 +0300

    homework 24.1 solution

---
+++ src/main/java/com/internet/shop/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,44 @@
+package com.internet.shop.dao.impl;
+
+import com.internet.shop.dao.ProductDao;
+import com.internet.shop.db.Storage;
+import com.internet.shop.lib.Dao;
+import com.internet.shop.model.Product;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class ProductDaoImpl implements ProductDao {
+    @Override
+    public Product create(Product product) {
+        Storage.addProduct(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> getById(Long productId) {
+        return Storage.products.stream()
+                .filter(product -> product.getId().equals(productId))
+                .findFirst();
+    }
+
+    @Override
+    public Product update(Product product) {
+        for (int i = 0; i < Storage.products.size(); i++) {
+            if (Storage.products.get(i).getId().equals(product.getId())) {
+                Storage.products.set(i, product);
+            }
+        }
+        return product;
+    }
+
+    @Override
+    public boolean delete(Product product) {
+        return Storage.products.removeIf(p -> p.getId().equals(product.getId()));
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return Storage.products;
+    }
+}
---
+++ src/main/java/com/internet/shop/model/Product.java
@@ -0,0 +1,47 @@
+package com.internet.shop.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = BigDecimal.valueOf(price);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", price=" + price
+                + '}';
+    }
+}
